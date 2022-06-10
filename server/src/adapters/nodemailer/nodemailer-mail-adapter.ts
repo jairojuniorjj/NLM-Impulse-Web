0ec999340e99e48e436e133634f21500f361da02
Jairo Junior <jairojunior.bastos@ymail.com>
2022-06-10 14:53:43 -0300

    Deploy

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "5d31fef77967ce",
+    pass: "5ecb65adcf6527"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Jairo Júnior <jairojunior.bastos@ymail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
